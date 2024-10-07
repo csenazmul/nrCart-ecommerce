a8472ddc78f159cab55c522ef03e8fb323c910bb
Nazmul Sheikh <csenazmul77@gmail.com>
2024-10-07 20:36:42 +0600

    update STATIC_ROOT

--- ecom_proj/urls.py
+++ ecom_proj/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, documents = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, documents = settings.STATIC_ROOT)
