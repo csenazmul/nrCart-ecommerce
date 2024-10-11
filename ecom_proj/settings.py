0910ffb4b20a1e61e6bf3a87ee5fb222f3e840be
Nazmul Sheikh <csenazmul77@gmail.com>
2024-10-11 12:05:58 +0600

    create store urls.py and update

--- ecom_proj/settings.py
+++ ecom_proj/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'blog',
 
     # Connect Module App
-    'anymail',
+    # 'anymail',
     # 'captcha',
 ]
 
@@ -129,7 +129,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
