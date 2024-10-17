ac7d5063baff7e3de209fe1d5f49e125405ce98c
Nazmul Sheikh <csenazmul77@gmail.com>
2024-10-17 15:33:53 +0600

    update store model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,169 @@
 from django.db import models
 
 # Create your models here.
+from shortuuid.django_fields import ShortUUIDField
+from django.utils import timezone
+from django.utils.text import slugify
+from django_ckeditor_5.fields import CKEditor5Field
+
+from userauths import models as user_models
+
+import shortuuid
+
+STATUS = (
+    ("Published", "Published"),
+    ("Draft", "Draft"),
+    ("Disabled", "Disabled"),
+)
+
+PAYMENT_STATUS = (
+    ("Paid", "Paid"),
+    ("Processing", "Processing"),
+    ("Failed", "Failed"),
+)
+
+PAYMENT_METHOD = (
+    ("PayPal", "PayPal"),
+    ("Strip", "Strip"),
+    ("Flutterwave", "Flutterwave"),
+    ("Paysatck", "Paysatck"),
+    ("RazorPay", "RazorPay"),
+    ("Bkash", "Bkash"),
+    ("Nagod", "Nagod"),
+)
+
+ORDER_STATUS = (
+    ("Pending", "Pending"),
+    ("Processing", "Processing"),
+    ("Shipped", "Shipped"),
+    ("Flufilled", "Flufilled"),
+    ("Cancelled", "Cancelled"),
+)
+
+SHIPPING_SERVICE = (
+    ("DHL", "DHL"),
+    ("FedX", "FedX"),
+    ("RedX", "RedX"),
+    ("UPS", "UPS"),
+    ("GIG Logistics", "GIG Logistics"),
+)
+
+RATING = (
+    (1, "⭐☆☆☆☆"),
+    (2, "⭐⭐☆☆☆"),
+    (3, "⭐⭐⭐☆☆"),
+    (4, "⭐⭐⭐⭐☆"),
+    (5, "⭐⭐⭐⭐⭐"),
+)
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to="image", null=True, blank=True)
+    slug = models.SlugField(unique=True) # Financial-advice
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name_plural = "Categories"
+        ordering = ['title']
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.FileField(upload_to="images", blank=True, null=True, default="product.jpg")
+    description = CKEditor5Field('Text', config_name='extends')
+
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+
+    price = models.DecimalField(max_digits=12, decimal_places=2, default=0.00, null=True, blank=True, verbose_name="Sale Price")
+    regular_price = models.DecimalField(max_digits=12, decimal_places=2, default=0.00, null=True, blank=True, verbose_name="Regular Price")
+
+    stock = models.PositiveIntegerField(default=0, null=True, blank=True)
+    shipping = models.DecimalField(max_digits=12, decimal_places=2, default=0.00, null=True, blank=True, verbose_name="Shipping Amount")
+
+    status = models.CharField(choices=STATUS, max_length=50, default="Published")
+    featured = models.BooleanField(default=False, verbose_name="Marketplace Featured")
+
+    vendor = models.ForeignKey(user_models.User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    sku = ShortUUIDField(unique=True, length=5, max_length=50, prefix="SKU", alphabet="1234567890")
+    slug = models.SlugField(null=True, blank=True)
+
+    date = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ['-id']
+        verbose_name_plural = "Products"
+
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name) + "-" + str(shortuuid.uuid().lower()[:2])
+
+        super(Product, self).save(*args, **kwargs)
+
+
+class Variant(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=1000, verbose_name="Variant Name", null=True, blank=True)
+
+    def items(self):
+        return VariantItem.objects.filter(variant=self)
+    
+    def __str__(self):
+        return self.name
+    
+
+class VariantItem(models.Model):
+    variant = models.ForeignKey(Variant, on_delete=models.CASCADE, related_name='variant_items')
+    title = models.CharField(max_length=1000, verbose_name="Item Title", null=True, blank=True)
+    content = models.CharField(max_length=1000, verbose_name="Item content", null=True, blank=True)
+
+    def __str__(self):
+        return self.variant.name
+    
+
+class Gallery(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    image = models.FileField(upload_to="images", default="gallery.jpg")
+    gallery_id = ShortUUIDField(length=6, max_length=10, alphabet="1234567890")
+
+    def __str__(self):
+        return f"{self.product.name} - image"
+    
+
+class Cart(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(user_models.User, on_delete=models.SET_NULL, null=True, blank=True)
+    qty = models.PositiveIntegerField(default=0, null=True, blank=True)
+    price = models.DecimalField(decimal_places=2, max_digits=12, default=0.00, null=True, blank=True)
+    sub_total = models.DecimalField(decimal_places=2, max_digits=12, default=0.00, null=True, blank=True)
+    shipping = models.DecimalField(decimal_places=2, max_digits=12, default=0.00, null=True, blank=True)
+    tax = models.DecimalField(decimal_places=2, max_digits=12, default=0.00, null=True, blank=True)
+    total = models.DecimalField(decimal_places=2, max_digits=12, default=0.00, null=True, blank=True)
+    size = models.CharField(max_length=100, null=True, blank=True)
+    color = models.CharField(max_length=100, null=True, blank=True)
+    cart_id = models.CharField(max_length=1000, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.cart_id} - {self.product.name}"
+
+
+class Coupon(models.Model):
+    vendor = models.ForeignKey(user_models.User, on_delete=models.SET_NULL, null=True)
+    code = models.CharField(max_length=100)
+    discount = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.code
+    
+
+class Order(models.Model):
+    vendors = models.ManyToManyField(user_models.User, blank=True)
+    customer = models.ForeignKey(user_models.User, on_delete=models.SET_NULL, null=True, related_name="customer", blank=True)
+
+class OrderItem(models.Model):
+    pass
