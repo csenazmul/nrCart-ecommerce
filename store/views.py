68e88fcdd3c850d7bf1c1d21f5aef9261b8c0098
PythonBD House <pythonbdhouse@gmail.com>
2024-10-26 17:34:22 +0600

    update rating and templates

--- store/views.py
+++ store/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from store import models as store_models
+
+
 # Create your views here.
 
 def index(request):
-    return render(request, "store/index.html")
+    products = store_models.Product.objects.filter(status="Published")
+    print(products)
+    return render(request, "store/index.html", {"products":products})
